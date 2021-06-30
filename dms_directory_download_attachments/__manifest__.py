1ba6419cd78363e7baf391877b7e4cfed2d0cbba
Eduardo de Miguel <shide.shugo@gmail.com>
2021-06-30 20:57:57 +0200

    DMS Directory download all attachments

---
+++ dms_directory_download_attachments/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Eduardo de Miguel (shide.shugo@gmail.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'DMS Directory: Download attachments',
+    'summary': 'Download attachments from a dms directory',
+    'version': '14.0.0',
+    'category': 'Services/Project',
+    'license': 'AGPL-3',
+    'author': 'Eduardo de Miguel',
+    'depends': [
+        'dms',
+    ],
+    'data': [
+        'views/dms_directory_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
