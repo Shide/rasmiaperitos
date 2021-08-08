f1bf89debcec6b6de3018ddf7c2db18fed88260a
Eduardo de Miguel <shide.shugo@gmail.com>
2021-08-08 18:45:19 +0200

    View attachments of Project Tasks

--- project_task_download_attachments/models/project_task.py
+++ project_task_download_attachments/models/project_task.py
@@ -19,3 +19,12 @@ class ProjectTask(models.Model):
             'url': '/web/binary/download_document?tab_id=%s&title=%s' % (self.attachment_ids.ids, self.display_name),
             'target': 'new',
         }
+
+    def action_view_all_attachments(self):
+        attachments = self.mapped('attachment_ids')
+        if not attachments:
+            raise exceptions.UserError(_('No hay ningún adjunto que ver.'))
+
+        action = self.env.ref('base.action_attachment').read()[0]
+        action['domain'] = [('id', 'in', attachments.ids)]
+        return action
